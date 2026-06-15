a03c4c526ca6be8651bccbe2c1962c868223fe0d
renato matos <renato.matos79@gmail.com>
2026-06-15 15:23:57 +0100

    init

---
+++ shared-auth/src/auth.ts
@@ -0,0 +1,18 @@
+const TOKEN_KEY = "fake_token";
+
+export function isAuthenticated() {
+  return !!localStorage.getItem(TOKEN_KEY);
+}
+
+export function login() {
+  localStorage.setItem(TOKEN_KEY, "fake-token");
+  window.dispatchEvent(new Event("auth-changed"));
+}
+
+export function logout() {
+  localStorage.removeItem(TOKEN_KEY);
+  window.dispatchEvent(new Event("auth-changed"));
+
+  window.history.pushState(null, "", "/");
+  window.dispatchEvent(new PopStateEvent("popstate"));
+}
